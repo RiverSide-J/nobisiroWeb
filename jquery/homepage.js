ce528ecbb68874aab4c35eface9e67e1627e3612
RiverSide-J <twst.kwbn@gmail.com>
2021-08-06 11:08:51 +0900

    Swiper(スライドショー）仮完成。

--- jquery/homepage.js
+++ jquery/homepage.js
@@ -1,22 +1,7 @@
-const swiper = new Swiper('.swiper-container', {
-  // スライドメニューの表示方法
-  direction: 'horizontal',
-  loop: true,
-  autoHeight: true,
-  autoplay: {
-    delay: 1000,
-  },
-  // スライドのページネーション表示
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  // 「次へ」や「戻る」のアイコン表示
+var mySwiper = new Swiper('.swiper-container', 
+{
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // スクロールバーの表示
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
 });
